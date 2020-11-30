a0cc6a1896a6dac121c40ffedf2519ffbd1c1f03
Michel Tosu <michel@tosu.se>
2020-11-30 21:01:16 +0100

    feature: solution for day one 2019

--- src/test/java/se/tosu/aoc2019/dayone/FuelCalculatorTest.java
+++ src/test/java/se/tosu/aoc2019/dayone/FuelCalculatorTest.java
@@ -2,7 +2,7 @@ package se.tosu.aoc2019.dayone;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,6 +10,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 class FuelCalculatorTest {
+
+    private final FuelCalculator fuelCalculator = new FuelCalculator();
+
     @Test
     void calculateFuelForMass12Returns2() {
         int mass = 12;
@@ -23,17 +26,33 @@ class FuelCalculatorTest {
     void calculateFuelForMass100756Returns33583() {
         int mass = 100756;
 
-        int fuel = new FuelCalculator().returnFuelNeededForMass(mass);
+        int fuel = fuelCalculator.returnFuelNeededForMass(mass);
 
         assertThat(fuel, is(33583));
     }
 
     @Test
     void calculateSumOfFuelForTwoModules() {
-        Stream<Integer> moduleMasses = Stream.of(1969, 100756);
+        List<Integer> moduleMasses = Stream.of(1969, 100756).collect(Collectors.toList());
 
-        int fuel = new FuelCalculator().returnFuelNeededForModules(moduleMasses.collect(Collectors.toList()));
+        int fuel = fuelCalculator.returnFuelNeededForModules(moduleMasses);
 
         assertThat(fuel, is(34237));
+    }
+
+    @Test
+    void calculateFuelNeededIncludingFuelWeight() {
+        int actual = fuelCalculator.returnFuelNeededForModuleAndFuel(100756);
+
+        assertThat(actual, is(50346));
+    }
+
+    @Test
+    void calculateSumOfFuelNeedeForModuleAndFuelWeightForTwoModules() {
+        List<Integer> moduleMasses = Stream.of(1969, 100756).collect(Collectors.toList());
+
+        int actual = fuelCalculator.returnFuelNeededForModulesAndTheirFuel(moduleMasses);
+
+        assertThat(actual, is(654 + 33583));
     }
 }
