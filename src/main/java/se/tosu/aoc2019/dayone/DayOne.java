d2df55dcc37a7d881726b9d7ea623d7dd8b23551
Michel Tosu <michel@tosu.se>
2020-11-30 21:01:16 +0100

    feature: solution for task 2 day one 2019

--- src/main/java/se/tosu/aoc2019/dayone/DayOne.java
+++ src/main/java/se/tosu/aoc2019/dayone/DayOne.java
@@ -7,8 +7,12 @@ import java.util.List;
 public class DayOne {
     public static void main(String[] args) {
         List<Integer> moduleWeights = new Input().getListOfIntegersFromInputFile("2019/1.txt");
-        int totalNeededFuel = new FuelCalculator().returnFuelNeededForModules(moduleWeights);
+        FuelCalculator fuelCalculator = new FuelCalculator();
+        int totalNeededFuel = fuelCalculator.returnFuelNeededForModules(moduleWeights);
+        int totalNeededFuelIncludingFuelWeight = fuelCalculator.returnFuelNeededForModulesAndTheirFuel(moduleWeights);
 
         System.out.printf("The total amount of fuel needed is %d%n", totalNeededFuel);
+        System.out.printf("The total amount of fuel needed is when calculating" +
+                "fuel for module weights including fuel weight: %d%n", totalNeededFuelIncludingFuelWeight);
     }
 }
--- src/main/java/se/tosu/aoc2019/dayone/FuelCalculator.java
+++ src/main/java/se/tosu/aoc2019/dayone/FuelCalculator.java
@@ -5,12 +5,29 @@ import java.util.List;
 public class FuelCalculator {
 
     public int returnFuelNeededForMass(int mass) {
-        return (mass / 3) - 2;
+        int fuel = (mass / 3) - 2;
+        return Math.max(fuel, 0);
     }
 
     public int returnFuelNeededForModules(List<Integer> moduleMasses) {
         return moduleMasses.stream()
                 .map(this::returnFuelNeededForMass)
+                .reduce(Integer::sum)
+                .orElseThrow();
+    }
+
+    public int returnFuelNeededForModuleAndFuel(int i) {
+        if (i <= 0) {
+            return 0;
+        } else {
+            int fuel = returnFuelNeededForMass(i);
+            return fuel + returnFuelNeededForModuleAndFuel(fuel);
+        }
+    }
+
+    public int returnFuelNeededForModulesAndTheirFuel(List<Integer> moduleMasses) {
+        return moduleMasses.stream()
+                .map(this::returnFuelNeededForModuleAndFuel)
                 .reduce(Integer::sum)
                 .orElseThrow();
     }
