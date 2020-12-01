939e0fccc3c6cd81c4d9e3d40c12aeee76159081
Michel Tosu <michel@tosu.se>
2020-12-01 07:03:48 +0100

    refactor: simplify list creation

--- src/main/java/se/tosu/aoc/y2020/dayone/ExpenseReport.java
+++ src/main/java/se/tosu/aoc/y2020/dayone/ExpenseReport.java
@@ -1,7 +1,7 @@
 package se.tosu.aoc.y2020.dayone;
 
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 public class ExpenseReport {
@@ -12,10 +12,7 @@ public class ExpenseReport {
             for (int entryB :
                     entries) {
                 if (entryA + entryB == 2020) {
-                    List<Integer> list = new LinkedList<>();
-                    list.add(entryA);
-                    list.add(entryB);
-                    return list;
+                    return Arrays.asList(entryA, entryB);
                 }
             }
         }
@@ -24,6 +21,7 @@ public class ExpenseReport {
     }
 
     public List<Integer> findThreeIntsThatSumTo2020(List<Integer> entries) {
+
         for (int entryA :
                 entries) {
             for (int entryB :
@@ -31,11 +29,7 @@ public class ExpenseReport {
                 for (int entryC :
                         entries) {
                     if (entryA + entryB + entryC == 2020) {
-                        List<Integer> list = new LinkedList<>();
-                        list.add(entryA);
-                        list.add(entryB);
-                        list.add(entryC);
-                        return list;
+                        return Arrays.asList(entryA, entryB, entryC);
                     }
                 }
             }
